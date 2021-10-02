54467186fe0e75116c531c225639e73c0eaa0d2a
Tung Huynh <19522492@gm.uit.edu.vn>
2021-10-02 15:54:09 +0700

    update report

--- Assignment_1/game.py
+++ Assignment_1/game.py
@@ -109,9 +109,9 @@ class Game:
         return nb_missing_target == 0
 
     def auto_move(self):
-        # strategy = get_move(self.level.structure[:-1], self.level.position_player, 'dfs')
+        strategy = get_move(self.level.structure[:-1], self.level.position_player, 'dfs')
         # strategy = get_move(self.level.structure[:-1], self.level.position_player, 'bfs')
-        strategy = get_move(self.level.structure[:-1], self.level.position_player, 'ucs')
+        # strategy = get_move(self.level.structure[:-1], self.level.position_player, 'ucs')
         # with open("assets/sokobanSolver/Solverlevel_" + str(self.index_level) + ".txt", 'w+') as solver_file:
         #     for listitem in strategy:
         #         solver_file.write('%s, ' % listitem)
